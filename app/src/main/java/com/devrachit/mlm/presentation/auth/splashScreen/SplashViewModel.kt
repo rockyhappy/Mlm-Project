de50557d15883acb5b15f8d8ec2394b4ee86ab86
rockyhappy <rachit22153019@akgec.ac.in>
2024-11-20 02:26:33 +0530

    Login Screen Portrait Complete

--- app/src/main/java/com/devrachit/mlm/presentation/auth/splashScreen/SplashViewModel.kt
+++ app/src/main/java/com/devrachit/mlm/presentation/auth/splashScreen/SplashViewModel.kt
@@ -1,6 +1,12 @@
 package com.devrachit.mlm.presentation.auth.splashScreen
 
 import androidx.lifecycle.ViewModel
+import com.devrachit.mlm.data.local.DataStoreRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class SplashViewModel:ViewModel() {
+@HiltViewModel
+class SplashViewModel  @Inject constructor(
+    private val dataStoreRepository: DataStoreRepository,
+) :ViewModel() {
 }
